a5417615aadce57e7c9dfed78c076816998601f5
JupSilv <41308597+JupSilv@users.noreply.github.com>
2024-06-25 09:12:31 -0300

    Add files via upload
    Versionamento de cod/atualização.

---
+++ TesteBotTelegram/Controllers/TelegramController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteBotTelegram.Service;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TesteBotTelegram.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelegramController : ControllerBase
+    {
+        private readonly ITelegramService _telegramService;
+
+        public TelegramController(ITelegramService telegramService)
+        {
+            _telegramService = telegramService;
+        }
+
+        // GET: api/<TelegramController>
+        [HttpGet]
+        public void Get()
+        {
+            _telegramService.BotHandler();
+        }
+
+        // GET: api/<TelegramController>/send
+        [HttpGet("send")]
+        public void Send()
+        {
+            _telegramService.SendMessage();
+        }
+    }
+}
---
+++ TesteBotTelegram/Service/ITelegramService.cs
@@ -0,0 +1,8 @@
+﻿namespace TesteBotTelegram.Service
+{
+    public interface ITelegramService
+    {
+        void BotHandler();
+        void SendMessage();
+    }
+}
